e3e8f2c550c874fdfc7fb574c29babd19a443117
Fz Fldgr <fzfolad83@gmail.com>
2025-03-11 12:34:08 +0330

    registeretionCtrlC

--- back-end/project/config/apps/users/views.py
+++ back-end/project/config/apps/users/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.cintrib.auth.forms import UserCreationForm
+from django.urls import revers_lazy
 # Create your views here.
+
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = revers_lazy("login")
+    template_name = "apps/pages/registretion/register.html"
